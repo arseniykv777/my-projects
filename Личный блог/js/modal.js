7fc2e78175479c805e269872a10a9ad163238d11
arseniykv777 <senya_og@vk.com>
2025-03-18 00:37:21 +0500

    add modal window

---
+++ Личный блог/js/modal.js
@@ -0,0 +1,32 @@
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('modal-btn');
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+    document.body.classList.remove('add_scroll');
+    document.body.classList.add('no-scroll');
+    modal.classList.remove('show');
+    setTimeout(() => {
+        modal.classList.add('show')
+    }, 10)
+}
+
+span.onclick = function () {
+    document.body.classList.remove('no-scroll');
+    document.body.classList.add('add_scroll');
+    modal.classList.remove('show');
+    setTimeout(() => {
+        modal.style.display = 'none';
+    }, 500)
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        document.body.classList.add('add_scroll');
+        modal.classList.remove('show');
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 500)
+    }
+}
