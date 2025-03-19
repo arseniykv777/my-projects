92d1e77f5401a2a0bc1e3126dda0f77b195a6b20
arseniykv777 <senya_og@vk.com>
2025-03-19 20:34:14 +0500

    add modal window

--- Личный блог/js/modals.js
+++ Личный блог/js/modals.js
@@ -1,11 +1,15 @@
 var modal = document.getElementById('myModal');
 var modal2 = document.getElementById('myModal2');
+var modal3 = document.getElementById('myModal3');
 
 var btns = document.querySelectorAll('.modal-btn');
 var btn2 = document.getElementById('modal-btn2');
+var btn3 = document.getElementById('modal-btn3');
 
 var span = document.getElementsByClassName("close")[0];
 var span2 = document.getElementsByClassName("close2")[0];
+var span3 = document.getElementsByClassName("close3")[0];
+
 
 if (modal) {
     btns.forEach(btn => {
@@ -43,12 +47,34 @@ if (modal2) {
     }
 }
 
+if (modal3) {
+    btn3.onclick = function () {
+        modal3.style.display = 'flex';
+        document.body.classList.add('no-scroll');
+        modal3.classList.remove('show');
+        setTimeout(() => {
+            modal3.classList.add('show')
+        }, 10);
+    }
+
+    if (span3) {
+        span3.onclick = function () {
+            closeModal(modal3);
+        }
+    }
+}
+
+
+
 window.onclick = function(event) {
     if (modal && event.target == modal) {
         closeModal(modal);
     }
     if (modal2 && event.target == modal2) {
         closeModal(modal2);
+    }
+    if (modal3 && event.target == modal3) {
+        closeModal(modal3);
     }
 }
 
